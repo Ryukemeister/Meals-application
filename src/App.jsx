e7f8c7b82f96c1d661d81327e4dc49deb942d462
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-18 20:31:54 +0530

    Wrapping around contextProvider in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React, { useEffect, useContext } from "react";
+import Search from "./components/Search";
+import Favourites from "./components/Favourites";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
 import "./App.css";
 
 function App() {
+  /*
+  useEffect(() => {
+    const fetchData = async function () {
+      const response = await fetch(
+        "www.themealdb.com/api/json/v1/1/random.php"
+      );
+      const data = await data.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+*/
+
   return (
     <div className="App">
-      <h1 className="text-yellow-500 font-bold">Hii</h1>
+      <Search />
+      <Favourites />
+      <Meals />
+      <Modal />
     </div>
   );
 }
