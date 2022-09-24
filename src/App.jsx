8e5161404136260aae8cdbcaf64412b16ddf526b
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-24 21:09:44 +0530

    Commented out  some components

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Search />
-      <Favourites />
       <Meals />
-      <Modal />
+      <Favourites />
+      {/* <Modal />*/}
     </div>
   );
 }
