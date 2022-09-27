1d1321b24b81f8722fbf89027f7d4b231fe93773
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-27 19:14:52 +0530

    Added a showModal and closeModal feature to the modal which displays the selected meal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import { useGlobalContext } from "../Context";
 
 function Modal() {
-  const { setShowModal } = useGlobalContext();
-
-  function closeModal() {
-    setShowModal(false);
-  }
-
+  const { closeModal, selectedMeal } = useGlobalContext();
+  const {
+    idMeal: id,
+    strMeal: title,
+    strInstructions: instructions,
+    strMealThumb: img,
+  } = selectedMeal;
+  console.log(selectedMeal);
   return (
     <aside className="fixed transition-[0.3s ease-in-out all] top-0 left-0 w-[100%] h-[100%] grid z-50 place-content-center bg-[rgba(0,0,0,0.85)]">
       <div className="w-[80vw] max-w-[800px] h-[80vh] overflow-scroll bg-white rounded-sm">
-        <h1>Modal component</h1>
+        <img
+          className="w-[100%] h-[65%] object-center object-cover"
+          src={img}
+          alt="Modal meal image"
+        />
+        <h1 className="font-semibold font-mono text-xl pl-3 pt-2">
+          <span className="pr-[3.5px]">-</span>
+          {title}
+        </h1>
+        <p className="px-4 py-1">{instructions}</p>
         <button
           onClick={closeModal}
-          className="bg-red-500 text-white mr-3 font-medium px-3 py-[2px] text-base rounded-full shadow-md hover:bg-red-700 transition-[0.3s ease-in-out all]"
+          className="bg-red-500 text-white mr-3 font-medium px-3 py-1 mx-3 my-2 text-base rounded-full shadow-md hover:bg-red-700 transition-[0.3s ease-in-out all]"
         >
           Close
         </button>
