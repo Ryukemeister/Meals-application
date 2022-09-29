3e0545f5e66cce11ff34d52d5952ceb0851b3567
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-29 21:25:18 +0530

    Added a deleted favourite function for favourites

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useGlobalContext } from "../Context";
 
 function Favourites() {
-  const { favouriteMeals } = useGlobalContext();
+  const { favouriteMeals, deleteFavouriteMeal, selectMeal } =
+    useGlobalContext();
 
   const allFaveMeals = favouriteMeals.map((meal) => {
     const {
@@ -13,17 +14,32 @@ function Favourites() {
     } = meal;
 
     return (
-      <section className="my-4 w-40" key={id}>
-        <img src={image} className="w-32 h-32 rounded-full" alt="Meal image" />
-        <h1 className="align-center">{title}</h1>
-      </section>
+      <div className="flex flex-col items-center w-24" key={id}>
+        <img
+          src={image}
+          className="h-24 rounded-full cursor-pointer mb-1 border-2 border-red-500"
+          onClick={() => selectMeal(id, true)}
+          alt="Meal image"
+        />
+        <button
+          onClick={() => deleteFavouriteMeal(id)}
+          className="text-white hover:text-red-300 transition-[0.3s ease-in-out all]"
+        >
+          Remove
+        </button>
+      </div>
     );
   });
 
   return (
-    <div className="flex flex-wrap gap-10 px-12 py-5 my-5 bg-yellow-200">
-      {allFaveMeals}
-    </div>
+    <section className="flex flex-col flex-wrap py-8 bg-black">
+      <h1 className="font-semibold px-16 text-3xl mb-5 text-white">
+        Favourites
+      </h1>
+      <div className="flex flex-wrap px-16 gap-x-10 gap-y-5 mb-2">
+        {allFaveMeals}
+      </div>
+    </section>
   );
 }
 
