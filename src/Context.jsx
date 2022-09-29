648eecd6740c8e00c726f5bf9905247bcf053ae4
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-29 21:26:13 +0530

    Added a delted favourite and add favourite function

--- src/Context.jsx
+++ src/Context.jsx
@@ -6,11 +6,19 @@ const AppContext = createContext();
 const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
+const getFavouritesFromLocalStorage = function () {
+  const favourites = localStorage.getItem("favourites");
+
+  return favourites ? JSON.parse(localStorage.getItem("favourites")) : [];
+};
+
 const AppProvider = function ({ children }) {
   const [allMeals, setAllMeals] = useState([]);
   const [randomMeal, setRandomMeal] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [favouriteMeals, setFavouriteMeals] = useState([]);
+  const [favouriteMeals, setFavouriteMeals] = useState(
+    getFavouritesFromLocalStorage()
+  );
   const [searchTerm, setSearchTerm] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [selectedMeal, setSelectedMeal] = useState(null);
@@ -43,9 +51,11 @@ const AppProvider = function ({ children }) {
     "Vietnamese",
   ];
 
+  // console.log(favouriteMeals);
+
   const getMealByArea = function (area) {
     const meals = allMeals.filter((meal) => meal.strArea === area);
-    console.log(meals);
+    // console.log(meals);
   };
 
   getMealByArea("Mexican");
@@ -68,10 +78,13 @@ const AppProvider = function ({ children }) {
     fetchMeals(randomMealUrl);
   };
 
-  const selectMeal = function (idMeal, isFavourite) {
+  const selectMeal = function (idMeal, favouriteMeal) {
     let meal;
-
-    meal = allMeals.find((meal) => meal.idMeal === idMeal);
+    if (favouriteMeal) {
+      meal = favouriteMeals.find((meal) => meal.idMeal === idMeal);
+    } else {
+      meal = allMeals.find((meal) => meal.idMeal === idMeal);
+    }
 
     setShowModal(true);
     setSelectedMeal(meal);
@@ -81,6 +94,30 @@ const AppProvider = function ({ children }) {
   const closeModal = function () {
     setShowModal(false);
   };
+
+  const selectFavouriteMeal = function (idMeal) {
+    const alreadyFavourite = favouriteMeals.find(
+      (meal) => meal.idMeal === idMeal
+    );
+    if (alreadyFavourite) return;
+
+    const meal = allMeals.find((meal) => meal.idMeal === idMeal);
+    const updatedFavourites = [...favouriteMeals, meal];
+
+    setFavouriteMeals(updatedFavourites);
+    localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+  };
+
+  const deleteFavouriteMeal = function (idMeal) {
+    const updatedFavourites = favouriteMeals.filter(
+      (meal) => meal.idMeal !== idMeal
+    );
+
+    setFavouriteMeals(updatedFavourites);
+    localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+  };
+
+  // console.log(JSON.parse(localStorage.getItem("favouriteMeals")));
 
   // This useEffect only renders one time i.e. when the application loads
   useEffect(() => {
@@ -108,6 +145,8 @@ const AppProvider = function ({ children }) {
         selectedMeal,
         showModal,
         closeModal,
+        selectFavouriteMeal,
+        deleteFavouriteMeal,
       }}
     >
       {children}
