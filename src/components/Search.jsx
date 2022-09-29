69e4acf3ce84e592aba47d84afe3fba1c5ab381f
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-29 21:23:17 +0530

    Added a drop shadow to the nav and made it sticky

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -29,31 +29,44 @@ function Search() {
     fetchRandomMeal();
   }
 
+  /*
+  Some weird names for the meal app
+  swiggy, zomato, zwiggy, ziggy, swomato
+  */
+
   return (
-    <header className="flex">
-      <form className="mt-5 ml-12 mr-3 flex flex-wrap" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={handleChange}
-          value={inputText}
-          placeholder="Search your dish here..."
-          className="border-black border-[1.5px] rounded-full px-4 w-[350px] max-w-[350px] shadow-sm py-1 mx-3 mt-0 outline-none"
-        />
-        <button
-          type="submit"
-          className="bg-green-400 text-white mr-3 font-medium px-4 rounded-full shadow-md hover:bg-green-500 transition-[0.3s ease-in-out all]"
-          // onClick={handleClick}
+    <header className="flex justify-between w-[100%] shadow-md fixed top-0 left-0 bg-white">
+      <div className="my-5 ml-16 mr-3">
+        <h1 className="font-bold text-2xl">Zwiggy</h1>
+      </div>
+      <div className="">
+        <form
+          className="my-5 ml-12 mr-20 flex flex-wrap"
+          onSubmit={handleSubmit}
         >
-          Search
-        </button>
-        <button
-          type="button"
-          onClick={getRandomDish}
-          className="bg-yellow-400 text-white font-medium px-4 rounded-full shadow-md hover:bg-yellow-500 transition-[0.3s ease-in-out all]"
-        >
-          Surpise me
-        </button>
-      </form>
+          <input
+            type="text"
+            onChange={handleChange}
+            value={inputText}
+            placeholder="Search your dish here..."
+            className="border-black border-[1.5px] rounded-full px-4 w-[350px] max-w-[350px] shadow-sm py-1 mx-3 mt-0 outline-none"
+          />
+          <button
+            type="submit"
+            className="bg-green-400 text-white mr-3 font-medium px-4 rounded-full shadow-md hover:bg-green-500 transition-[0.3s ease-in-out all]"
+            // onClick={handleClick}
+          >
+            Search
+          </button>
+          <button
+            type="button"
+            onClick={getRandomDish}
+            className="bg-yellow-400 text-white font-medium px-4 rounded-full shadow-md hover:bg-yellow-500 transition-[0.3s ease-in-out all] mr-3"
+          >
+            Surpise me
+          </button>
+        </form>
+      </div>
     </header>
   );
 }
